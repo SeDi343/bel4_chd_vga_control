c5435c3c41395d22e18f5a50f86c8191dd98c844
Sebastian Dichler <sedi343@gmail.com>
2018-05-23 01:34:02 +0200

    Removed constant we dont need

--- vhdl/memory_control_1_architecture.vhd
+++ vhdl/memory_control_1_architecture.vhd
@@ -21,8 +21,6 @@ architecture memory_control_1_architecture of memory_control_1_entity is
 	constant H_VISIBLE_AREA			: std_logic_vector(9 downto 0) := "1010000000";					-- Visible Area					| 640
 	constant V_VISIBLE_AREA			: std_logic_vector(9 downto 0) := "0111100000";					-- Visible Area					| 480
 	constant H_VISIBLE_AREA_12	: std_logic_vector(9 downto 0) := "0101000000";					-- 1/2 of Visible Area	| 320
-	constant H_WHOLE_LINE				: std_logic_vector(9 downto 0) := "1100100000";					-- Whole Line						| 800
-	constant V_WHOLE_LINE				: std_logic_vector(9 downto 0) := "1000001101";					-- Whole Line						| 525
 	constant ROM_MAX_VALUE			: std_logic_vector(16 downto 0) := "10010101111111111";	-- Rom max addr value		| 76799
 
 	signal s_rom_current_val	: std_logic_vector(16 downto 0);			-- Rom Current Value - saves the  rom addressvalue if H-Sync Counter reaches 320
